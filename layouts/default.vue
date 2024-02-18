b849cbaf583cbc4c40d5a73e1b9de3bfa8bbfed8
Maxim Tresk <djtresk@gmail.com>
2024-02-18 15:35:06 +0500

    init

---
+++ layouts/default.vue
@@ -0,0 +1,58 @@
+<script setup>
+useHead({
+	htmlAttrs: {
+		lang: 'ru',
+	},
+	titleTemplate: (titleChunk) => {
+		return titleChunk ? `Tresk Sites — ${titleChunk}` : 'Tresk Sites'
+	},
+})
+</script>
+
+<template>
+	<div>
+		<Head>
+			<Meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=0" />
+			<Meta name="description" content="Создание сайтов и веб-приложений любой сложности" />
+		</Head>
+		<NuxtLoadingIndicator color="#ff565c" />
+		<div data-fullscreen class="wrapper">
+			<main class="page">
+				<slot />
+			</main>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+body {
+	background-color: #f5f5f5;
+
+	.lock & {
+		overflow: hidden;
+		overscroll-behavior: none;
+		touch-action: none;
+	}
+}
+
+[class*='__container'] {
+	@if $maxWidthContainer > 0 {
+		box-sizing: content-box;
+		max-width: rem($maxWidthContainer);
+		padding-right: rem(20);
+		padding-left: rem(20);
+		margin: 0 auto;
+	}
+}
+
+.wrapper {
+	display: flex;
+	flex-direction: column;
+	min-height: 100%;
+	overflow: hidden;
+
+	> main {
+		flex: 1 1 auto;
+	}
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+
+export default defineNuxtConfig({
+	devtools: { enabled: true },
+	css: ['@/assets/scss/fonts.scss', '@/assets/scss/null.scss'],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "@/assets/scss/globals.scss" as *;',
+				},
+			},
+		},
+		plugins: [ViteImageOptimizer({})],
+	},
+	nitro: {
+		compressPublicAssets: true,
+	},
+	postcss: {
+		plugins: {
+			autoprefixer: {},
+		},
+	},
+})

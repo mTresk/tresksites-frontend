1ffc44f23de314bdade52c21d0b585b96ed86971
Maxim Tresk <djtresk@gmail.com>
2024-03-23 22:17:21 +0500

    chore: update image size

--- pages/works/[slug].vue
+++ pages/works/[slug].vue
@@ -49,9 +49,9 @@ onMounted(() => {
 					<div v-html="content?.data?.html" />
 					<div v-if="content?.data?.files" class="work__image">
 						<picture>
-							<source media="(max-width: 800px)" :srcset="`${content?.data?.files?.imageWebpSm} 1x, ${content?.data?.files?.imageWebpSmX2} 2x`" type="image/webp">
+							<source media="(max-width: 479px)" :srcset="`${content?.data?.files?.imageWebpSm} 1x, ${content?.data?.files?.imageWebpSmX2} 2x`" type="image/webp">
 							<source
-								media="(min-width: 800px)"
+								media="(min-width: 480px)"
 								:srcset="`${content?.data?.files?.imageWebp} 1x, ${content?.data?.files?.imageWebpX2} 2x`"
 								type="image/webp"
 							>
